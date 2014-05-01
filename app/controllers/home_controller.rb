b117a260342cb4bb6b47a9265aefd5b93b2b5dd9
Kalakar Sahoo <ksahoo@navaratan.com>
2014-05-01 14:01:04 +0530

    added view option successfully

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
 	end
 
 	def sign_up
-		
+		@user 
 	end
 
 	def validate
@@ -23,14 +23,42 @@ class HomeController < ApplicationController
 	end
 
 	def view
-		@user = UserData.find_by(email: params[:user][:email], password: params[:user][:password])
-		if @user.nil?
-			@user = UserData.new(firstname: params[:user][:first_name],lastname: params[:user][:last_name], email: params[:user][:email], institution: params[:user][:institution], password: params[:user][:password])
-			@user.save()
-			@users = UserData.all
-			render ('index')
+		#@user = UserData.find_by(email: params[:user][:email], password: params[:user][:password])
+		#if @user.nil?
+			#@user = UserData.new(firstname: params[:user][:first_name],lastname: params[:user][:last_name], email: params[:user][:email], institution: params[:user][:institution], password: params[:user][:password])
+			#@user.save()
+			#@users = UserData.all
+			#render ('index')
+		#else
+			#redirect_to(:action => 'sign_up')
+		#end
+		@user = UserData.find(params[:id])
+	  if !@users.nil?  
+	  	render ('view')  
+	  end
+	end
+
+	def edit
+		@user = UserData.find(params[:id])
+	  if !@users.nil?  
+	  	render ('edit')
+	  end
+	end
+
+	def delete
+		
+	end
+
+	def index
+		@users = UserData.all
+	end
+
+	def update
+		@user = UserData.find(params[:id])
+		if @user.update_attributes(params.require(:user).permit(:firstname, :lastname, :email, :institution))
+			render ('view')
 		else
-			redirect_to(:action => 'sign_in')
+			render ('update')
 		end
 	end
 end
